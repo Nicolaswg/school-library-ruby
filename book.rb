b14c2bca2846f4a3fcfb28d4a11f5d216da67b6a
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-06 16:21:43 -0400

    Format code with rubocop

--- book.rb
+++ book.rb
@@ -2,8 +2,8 @@ require_relative './rental'
 
 class Book
   attr_accessor :title, :author
-	attr_reader :rentals
-	
+  attr_reader :rentals
+
   def initialize(title, author)
     @title = title
     @author = author
