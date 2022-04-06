65894f91d99abb8d520ae926ccf899da98c5978d
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-06 16:18:18 -0400

    Implement Belongs to relationships between Classroom and Student

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @id = Random.rand(1..100)
@@ -9,5 +11,10 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
