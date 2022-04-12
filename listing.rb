958ece2ce2d636e5355d0f65046761d4d6fb107b
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-11 22:09:27 -0500

    Fix linters inside listing.rb

--- listing.rb
+++ listing.rb
@@ -18,28 +18,27 @@ class Listing
     end
   end
 
+  def empty_message
+    puts 'No people found matching requested type'
+    nil
+  end
+
   # Type Student, Type Teacher
   def people_list_menu(students, teachers)
     puts 'Do you want to list students? (1) or a teacher? (2) [Input the number]:'
     people_option = gets.chomp
     case people_option
     when '1'
-      if students.empty?
-        puts 'No student added'
-        return nil
-      else
-        students.each_with_index do |student, index|
-          puts "#{index}-) [Students] Name: #{student.name}, ID: #{student.id}, Age: #{student.age}"
-        end
+      return empty_message if students.empty?
+
+      students.each_with_index do |student, index|
+        puts "#{index}-) [Students] Name: #{student.name}, ID: #{student.id}, Age: #{student.age}"
       end
     when '2'
-      if teachers.empty?
-        puts 'No teacher added'
-        return nil
-      else
-        teachers.each_with_index do |teacher, index|
-          puts "#{index}-) [Teachers] Name: #{teacher.name}, ID: #{teacher.id}, Age: #{teacher.age}"
-        end
+      return empty_message if teachers.empty?
+
+      teachers.each_with_index do |teacher, index|
+        puts "#{index}-) [Teachers] Name: #{teacher.name}, ID: #{teacher.id}, Age: #{teacher.age}"
       end
     end
     people_option.to_i
